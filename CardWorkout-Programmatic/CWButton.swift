2dae60d2bba4308e46ef5271a9362a262dcf1a22
Vika <vg.dev31@gmail.com>
2024-09-03 15:50:22 +0400

    Загрузка приложения CardWorkout

---
+++ CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,35 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Vika on 28.04.24.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(color: UIColor, title: String, systemImageName: String) {
+        super.init(frame: .zero)
+        
+        configuration = .tinted()
+        configuration?.title = title
+        configuration?.baseForegroundColor = color
+        configuration?.baseBackgroundColor = color
+        configuration?.cornerStyle = .medium
+        
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 5
+        configuration?.imagePlacement = .trailing
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
